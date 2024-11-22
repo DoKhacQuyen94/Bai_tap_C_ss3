73dad0b498f3a4a124c14cfd747da63937d9c866
Đỗ Khắc Quyền <quyendk.b24dtcn380@stu.ptit.edu.vn>
2024-11-22 10:42:10 +0700

    Update baitap7ss3.c

--- baitap7ss3.c
+++ baitap7ss3.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 int main() {
-   int numb1, numb2, numb3, numb4, sum;
-
-   printf("nhập numb1 : ");
-   scanf("%d", &numb1);
-   printf("nhập numb2 : ");
-   scanf("%d", &numb2);
-   printf("nhập numb3 : ");
-   scanf("%d", &numb3);
-   printf("nhập numb4 : ");
-   scanf("%d", &numb4);
-   
-   sum=numb1+numb2+numb3+numb4;
-   
-   printf("Tổng 4 số là: %.d\n", sum);
+   int number, sum, reverse,soHangNghin,soHangTram,soHangChuc,soHangDonVi;
+   printf("Nhập số nguyên có 4 chữ số: ");
+   scanf("%d",&number);
+   soHangNghin = number/1000;
+   soHangTram = (number/100)%10;
+   soHangChuc = (number/10)%10;
+   soHangDonVi = number %10;
+   sum=soHangNghin+soHangTram+soHangChuc+soHangDonVi;
+   reverse = soHangDonVi*100+soHangChuc*100+soHangTram*10+soHangNghin;
+   printf("Số người dùng nhập vào là: %d\n",nnumber);
+   printf("Tổng các số của số %d là: %d \n",number,sum);
+   printf("Só đảo ngược của số %d là: %d \n",number, reverse);
    return 0;
 }
 
